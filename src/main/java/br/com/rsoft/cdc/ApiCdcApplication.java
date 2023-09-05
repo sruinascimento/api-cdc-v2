2603b5ef57c9bfd56150241bf16f11b613c59af7
Rui Nascimento <sruy19@gmail.com>
2023-09-05 08:37:14 -0300

    feat: api cdc v2

---
+++ src/main/java/br/com/rsoft/cdc/ApiCdcApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsoft.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rsoft/cdc/ApiCdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rsoft.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
