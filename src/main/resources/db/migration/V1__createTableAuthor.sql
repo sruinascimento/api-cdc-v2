39febf70ed97179e69496d9fefd1b73315dc93e6
Rui Nascimento <sruy19@gmail.com>
2023-09-06 08:41:59 -0300

    refact: attribute personalDescription was changed data type to VARCHAR(400).

--- src/main/resources/db/migration/V1__createTableAuthor.sql
+++ src/main/resources/db/migration/V1__createTableAuthor.sql
@@ -3,6 +3,6 @@ CREATE TABLE authors
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY,
     name                 VARCHAR(255)        NOT NULL,
     email                VARCHAR(255) UNIQUE NOT NULL,
-    personal_description TEXT                NOT NULL,
+    personal_description VARCHAR(400)        NOT NULL,
     created_at           TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
